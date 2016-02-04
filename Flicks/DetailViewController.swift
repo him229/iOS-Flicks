6bfa238de860053c735c23c6e3fd94f925a322f9
Himank Yadav <him229@gmail.com>
2016-02-03 18:39:25 -0600

    scroll view complete

--- Flicks/DetailViewController.swift
+++ Flicks/DetailViewController.swift
@@ -12,17 +12,23 @@ class DetailViewController: UIViewController {
     
     var movie:NSDictionary!
 
+    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var posterimageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var overviewLabel: UILabel!
+    @IBOutlet weak var infoView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        scrollView.contentSize = CGSize(width: scrollView.frame.size.width , height: infoView.frame.origin.y + infoView.frame.size.height)
 
         // Do any additional setup after loading the view.
         
         titleLabel.text = movie["title"] as! String
         overviewLabel.text = movie["overview"] as! String
+        
+        overviewLabel.sizeToFit()
         
         if let imageChangingURL = movie["poster_path"] as? String{
             let imageURL = NSURL(string: "http://image.tmdb.org/t/p/w500" + imageChangingURL)
